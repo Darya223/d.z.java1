e33e7191a0366a48e12eb2e4c276fb6ffd50451b
Сексуальный пингвин <ghh@mail.ru>
2018-08-19 14:45:42 +0300

    Initial commit

---
+++ src/org/itstep/qaone/runner/TempoName.java
@@ -0,0 +1,13 @@
+package org.itstep.qaone.runner;
+
+public class TempoName {
+    public static void main(String[] args) {
+        int x;
+        x = 3;
+        int y;
+        y = 2;
+        int a;
+        a = (x + y) * 2;
+        System.out.println(a);
+    }
+}
